f519807a720871f4a256da00a9b04462a3e8d16d
Octavian Oncescu <octavian.oncescu@gmail.com>
2019-06-24 14:01:01 +0200

    Added mimalloc ffi

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,2 +1,7 @@
 // Copyright 2019 Octavian Oncescu
+use libc::{c_void, size_t};
 
+extern "C" {
+   fn mi_malloc_aligned(size: size_t, alignment: size_t) -> c_void;
+   fn mi_free(p: *const c_void) -> c_void;
+}
