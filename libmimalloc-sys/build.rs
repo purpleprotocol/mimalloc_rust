90646f1ba2b511476d91b2c69e0b8d2bf73f6bd7
Octavian Oncescu <octavian.oncescu@gmail.com>
2019-06-24 21:35:05 +0200

    Fixed build script

--- libmimalloc-sys/build.rs
+++ libmimalloc-sys/build.rs
@@ -4,15 +4,29 @@ use cmake::Config;
 fn main() {
     let dst = Config::new("c_src/mimalloc")    
         .build();
+
+    dst.push("build");
+
     println!("cargo:rustc-link-search=native={}", dst.display());
-    //println!("cargo:rustc-link-lib=static=libmimalloc");
+    if cfg!(debug_assertions) {
+        println!("cargo:rustc-link-lib=static=mimalloc-debug");
+    } else {
+        println!("cargo:rustc-link-lib=static=mimalloc");
+    }
 }
 
 #[cfg(not(feature = "no_secure"))]
 fn main() {
-    let dst = Config::new("c_src/mimalloc")
+    let mut dst = Config::new("c_src/mimalloc")
         .define("SECURE", "ON")    
         .build();
+
+    dst.push("build");
+    
     println!("cargo:rustc-link-search=native={}", dst.display());
-    //println!("cargo:rustc-link-lib=static=libmimalloc");
+    if cfg!(debug_assertions) {
+        println!("cargo:rustc-link-lib=static=mimalloc-debug");
+    } else {
+        println!("cargo:rustc-link-lib=static=mimalloc");
+    }
 }
