a5d806cf1cd51b3ff4c43b1f5cfdb743f84708b6
Octavian Oncescu <octavian.oncescu@gmail.com>
2019-06-25 12:24:17 +0200

    Added minimum alignment

--- libmimalloc-sys/src/lib.rs
+++ libmimalloc-sys/src/lib.rs
@@ -3,6 +3,8 @@
 use libc::{c_void, size_t};
 
 extern "C" {
+   pub fn mi_zalloc_aligned(size: size_t, alignment: size_t) -> *const c_void;
    pub fn mi_malloc_aligned(size: size_t, alignment: size_t) -> *const c_void;
+   pub fn mi_realloc_aligned(p: *const c_void, size: size_t, alignment: size_t) -> *const c_void;
    pub fn mi_free(p: *const c_void) -> c_void;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -24,9 +24,29 @@
 extern crate libmimalloc_sys as ffi;
 
 use std::alloc::{GlobalAlloc, Layout};
-use std::ptr::null_mut;
 use libc::c_void;
 use ffi::*;
+
+// Copied from https://github.com/rust-lang/rust/blob/master/src/libstd/sys_common/alloc.rs
+#[cfg(all(any(
+    target_arch = "x86",
+    target_arch = "arm",
+    target_arch = "mips",
+    target_arch = "powerpc",
+    target_arch = "powerpc64",
+    target_arch = "asmjs",
+    target_arch = "wasm32"
+)))]
+const MIN_ALIGN: usize = 8;
+
+#[cfg(all(any(
+    target_arch = "x86_64",
+    target_arch = "aarch64",
+    target_arch = "mips64",
+    target_arch = "s390x",
+    target_arch = "sparc64"
+)))]
+const MIN_ALIGN: usize = 16;
 
 /// Drop-in mimalloc global allocator.
 /// 
@@ -40,12 +60,41 @@ use ffi::*;
 pub struct MiMalloc;
 
 unsafe impl GlobalAlloc for MiMalloc {
-    unsafe fn alloc(&self, layout: Layout) -> *mut u8 { 
-        mi_malloc_aligned(layout.size(), layout.align());
-        null_mut()
+    #[inline]
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let align = if layout.align() > MIN_ALIGN {
+            layout.align()
+        } else {
+            MIN_ALIGN
+        };
+
+        mi_malloc_aligned(layout.size(), align) as *mut u8
+    }
+
+    #[inline]
+    unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
+        let align = if layout.align() > MIN_ALIGN {
+            layout.align()
+        } else {
+            MIN_ALIGN
+        };
+        
+        mi_zalloc_aligned(layout.size(), align) as *mut u8
     }
     
+    #[inline]
     unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
         mi_free(ptr as *const c_void);
+    }
+
+    #[inline]
+    unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
+        let align = if layout.align() > MIN_ALIGN {
+            layout.align()
+        } else {
+            MIN_ALIGN
+        };
+        
+        mi_realloc_aligned(ptr as *const c_void, new_size, align) as *mut u8
     }
 }
