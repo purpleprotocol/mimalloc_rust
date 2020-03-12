4c0dc5dcbf17ddfb37dcc2902f6eff429d81c7b9
Andronik Ordian <write@reusable.software>
2020-03-12 11:42:31 +0100

    libmimalloc-sys: fixup merge

--- libmimalloc-sys/src/lib.rs
+++ libmimalloc-sys/src/lib.rs
@@ -3,13 +3,13 @@
 use core::ffi::c_void;
 
 extern "C" {
-    pub fn mi_zalloc(size: usize) -> *const c_void;
-    pub fn mi_malloc(size: usize) -> *const c_void;
-    pub fn mi_realloc(p: *const c_void, size: usize) -> *const c_void;
-    pub fn mi_zalloc_aligned(size: usize, alignment: usize) -> *const c_void;
-    pub fn mi_malloc_aligned(size: usize, alignment: usize) -> *const c_void;
-    pub fn mi_realloc_aligned(p: *const c_void, size: usize, alignment: usize) -> *const c_void;
-    pub fn mi_free(p: *const c_void) -> c_void;
+    pub fn mi_zalloc(size: usize) -> *mut c_void;
+    pub fn mi_malloc(size: usize) -> *mut c_void;
+    pub fn mi_realloc(p: *mut c_void, size: usize) -> *mut c_void;
+    pub fn mi_zalloc_aligned(size: usize, alignment: usize) -> *mut c_void;
+    pub fn mi_malloc_aligned(size: usize, alignment: usize) -> *mut c_void;
+    pub fn mi_realloc_aligned(p: *mut c_void, size: usize, alignment: usize) -> *mut c_void;
+    pub fn mi_free(p: *mut c_void) -> c_void;
     pub fn mi_usable_size(p: *mut c_void) -> usize;
 }
 
